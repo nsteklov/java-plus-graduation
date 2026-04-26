4706bc311db2569536603b1554a885a88b0027ef
Николай Стеклов <stenik@yandex.ru>
2026-04-26 15:25:43 +0300

    spring-cloud

--- stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
