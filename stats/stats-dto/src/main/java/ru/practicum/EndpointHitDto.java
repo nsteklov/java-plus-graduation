a2937d7dacf04b77dd063492616dbc2bbd68eec2
Николай Стеклов <stenik@yandex.ru>
2026-05-17 23:56:10 +0300

    recommendations

---
+++ stats/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -0,0 +1,48 @@
+package ru.practicum;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHitDto {
+
+    private Long id;
+
+    @NotBlank(message = "Название приложения не может быть пустым")
+    private String app;
+
+    @NotBlank(message = "URI не может быть пустым")
+    private String uri;
+
+    @NotBlank(message = "IP-адрес не может быть пустым")
+    @Pattern(
+            regexp =  "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$|" +
+                    "^(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|" +
+                    "([0-9a-fA-F]{1,4}:){1,7}:|" +
+                    "([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|" +
+                    "([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|" +
+                    "([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|" +
+                    "([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|" +
+                    "([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|" +
+                    "[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|" +
+                    ":((:[0-9a-fA-F]{1,4}){1,7}|:)|" +
+                    "fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|" +
+                    "::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3}" +
+                    "(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|" +
+                    "([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3}" +
+                    "(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$",
+            message = "Неверный формат IP-адреса"
+    )
+    private String ip;
+
+    @NotNull(message = "Время запроса не может быть пустым")
+    private String timestamp;
+}
